a6ccda39b9ea7a8340435d2f8f6c5b5ed2ccbcfe
Mtrebi <marianotrebino@gmail.com>
2017-03-31 11:24:13 +0200

    Fixed random generation to use  a non-deterministic 32 bit seed

--- src/main.cpp
+++ src/main.cpp
@@ -21,9 +21,10 @@ struct Matrix {
   }
 
   void initialize_random() {
-    std::default_random_engine generator(time(NULL));
-    std::uniform_real_distribution<double> distribution(-1e9, 1e9);
-    auto random = std::bind(distribution, generator);
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<double> dist(-1e9, -1e9);
+    auto random = std::bind(dist, mt);
     elements = new float*[MATRIX_SIZE];
     for (int i = 0; i < MATRIX_SIZE; ++i) {
       elements[i] = new float[MATRIX_SIZE];
@@ -62,6 +63,8 @@ int main() {
 
   //mainthread_execution(r, m1, m2);
   multithreading_execution(r, m1, m2);
+
+  //r.print();
   Sleep(100000);
 }
 
